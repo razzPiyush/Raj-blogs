cc24cb8191654dc85fe45f0727a722fd471001d9
Piyush Raj <106405216+razzPiyush@users.noreply.github.com>
2023-11-29 23:36:08 +0530

    Delete MernStack-Blog-App-Frontend directory

--- MernStack-Blog-App-Frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return ( 
-        <footer className="footer">
-            <p className="copyright">Copyright © 2022 RajBlogs.com | All Rights Reserved</p>
-        </footer>
-     );
-}
- 
-export default Footer;
--- MernStack-Blog-App-Frontend/src/components/utils.js
+++
@@ -1,7 +0,0 @@
-import { makeStyles } from "@mui/styles";
-import BlogDetail from "./BlogDetail";
-export const useStyles = makeStyles({
-  font: {
-     fontFamily: "Roboto !important",
-  },
-});
